ab4fef297de12b2d537af5fd4803cb2f15d80bef
DenKrishtaf25 <dkrishtaf98@gmail.com>
2025-09-22 19:34:43 +0300

    авторизация + юзер

---
+++ src/user/user.service.ts
@@ -0,0 +1,72 @@
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+import { AuthDto } from '../auth/dto/auth.dto'
+import { UserDto } from './user.dto'
+import { hash } from 'argon2'
+
+@Injectable()
+export class UserService {
+	constructor(private prisma: PrismaService) {
+	}
+
+	getById(id: string) {
+		return this.prisma.user.findUnique({
+			where: { id },
+			include: {
+				clients: true
+			}
+		})
+	}
+
+	getByEmail(email: string) {
+		return this.prisma.user.findUnique({
+			where: { email }
+		})
+	}
+
+	async getProfile(id: string) {
+		const profile = await this.getById(id)
+
+		if (!profile) {
+			throw new Error('User not found');
+		}
+
+		const totalClients= profile.clients.length
+		
+		const { password, ...rest } = profile
+
+		return {
+			user: rest,
+			statistics: [
+				{label: 'Total', value: totalClients},
+			]
+		}
+	}
+
+	async create(dto: AuthDto) {
+		const user = {
+			email: dto.email,
+			name: '',
+			password: await hash(dto.password)
+		}
+
+		return this.prisma.user.create({
+			data: user
+		})
+	}
+
+	async update(id: string, dto: UserDto) {
+		let data = dto
+		if (dto.password) {
+			data = { ...dto, password: await hash(dto.password) }
+		}
+		return this.prisma.user.update({
+			where: { id },
+			data,
+			select: {
+				name: true,
+				email: true
+			}
+		})
+	}
+}
